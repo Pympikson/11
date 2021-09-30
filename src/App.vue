41bee8f0fac8a074f572854233f56de4b7e1d87b
Pympikson <dslui@yandex.ru>
2021-09-30 20:21:11 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-header :data-dark="mode" @topDark="a"/>
+  <v-main :data-dark="mode"/>
+  <v-footer/>
+</template>
+
+<script>
+import VHeader from "./components/vHeader.vue";
+import vMain from "./components/vMain.vue";
+import vFooter from "./components/vFooter.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      mode: localStorage.dark,
+    };
+  },
+  methods:{
+    a(){
+      this.mode = localStorage.dark;
+    }
+  },
+  components: {
+    VHeader,
+    vMain,
+    vFooter
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+}
+</style>
